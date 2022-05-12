bc5182f51e4097dc9973df2853258b62c0e958fc
Dharamveer <workfordharamveer@gmail.com>
2022-05-12 22:33:47 +0530

    color change on button click

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 let input = document.querySelector(".number");
 let button = document.querySelector(".mainButton");
-let inputValue;
+let inputValue ;
+button.disabled=true;
 const inputBlur = (e) => {
- 
-  inputValue = e.target.value;
+  if (e.target.value.toString().length ==9) {
+    button.disabled=false;
+    inputValue = e.target.value;
+    input.classList.remove('red');
+    
+  }
+  else{
+    input.classList.add('red');
+  }
 };
 const buttonHandler = () => {
-    window.open("https://wa.me/+91"+inputValue,"_self");
+  if (inputValue) {
+    window.open("https://wa.me/+91" + inputValue, "_self");
+  }
 };
 
-input.addEventListener("blur", inputBlur);
-button.addEventListener('click',buttonHandler);
-
+input.addEventListener("keydown", inputBlur);
+button.addEventListener("click", buttonHandler);
