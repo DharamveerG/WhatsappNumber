a035181931506ac5f56dfd6f58481d23a523362f
Dharamveer <workfordharamveer@gmail.com>
2022-05-16 18:06:39 +0530

    darkmode

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
+
+let storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
 let input = document.querySelector(".number");
-let button = document.querySelector(".mainButton");
+
 let mainDiv =document.querySelector('.main-div');
+let darkMode=document.querySelector('.darkmode');
+
+
 
 let inputValue;
-button.disabled=true;
+let darkVariable=true;
 const inputBlur = (e) => {
   inputValue = e.target.value;
-  console.log(inputValue);
-  
+
   if (inputValue.toString().length ===10) {
-    button.disabled=false;
     input.classList.remove('red');
-    
   }
   else{
-    button.disabled=true;
-    input.classList.add('red');
+    input.classList.add('red'); 
   }
 };
 
 const buttonHandler = () => {
-  
   if (inputValue.toString().length ===10) {
-    window.open("https://wa.me/+91" + inputValue, "_blank");
+    window.open("https://wa.me/+91" + inputValue, "_self");
   }
-  
 };
+
+const darkModeHandler=()=>{
+ let currentTheme = document.documentElement.getAttribute("data-theme");
+ let targetTheme;
+
+  if (currentTheme === "light") {
+      targetTheme = "dark";
+  }else{
+     targetTheme = "light";
+  }
+
+  document.documentElement.setAttribute('data-theme', targetTheme)
+  localStorage.setItem('theme', targetTheme);
+}
 
 input.addEventListener("keyup", inputBlur);
 mainDiv.addEventListener("sumbit", buttonHandler);
+darkMode.addEventListener('click',darkModeHandler)
+
