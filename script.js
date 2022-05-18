ae12ea40553b1245899c499de56966f08cd0c91f
Dharamveer <workfordharamveer@gmail.com>
2022-05-18 17:45:17 +0530

    2.1

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
+//for themes to change how the user want
 
-let storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+let storedTheme =
+  localStorage.getItem("theme") ||
+  (window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light");
 if (storedTheme)
-    document.documentElement.setAttribute('data-theme', storedTheme)
+  document.documentElement.setAttribute("data-theme", storedTheme);
 
 let input = document.querySelector(".number");
-
-let mainDiv =document.querySelector('.main-div');
-let darkMode=document.querySelector('.darkmode');
-
-
+let mainDiv = document.querySelector(".main-div");
+let darkMode = document.querySelector("#switch");
 
 let inputValue;
-let darkVariable=true;
+
 const inputBlur = (e) => {
   inputValue = e.target.value;
-
-  if (inputValue.toString().length ===10) {
-    input.classList.remove('red');
-  }
-  else{
-    input.classList.add('red'); 
+  if (inputValue.toString().length === 10) {
+    input.classList.remove("red");
+  } else {
+    input.classList.add("red");
   }
 };
 
 const buttonHandler = (e) => {
- 
- if(e.key=="Enter" && inputValue.toString().length===10){
-   window.open("https://wa.me/+91"+inputValue,"_self");
- }
-  
+  if (e.key == "Enter" && inputValue.toString().length === 10) {
+    window.open("https://wa.me/+91" + inputValue, "_self");
+  }
 };
 
-const darkModeHandler=()=>{
- let currentTheme = document.documentElement.getAttribute("data-theme");
- let targetTheme;
+const darkModeHandler = () => {
+  let currentTheme = document.documentElement.getAttribute("data-theme");
+  let targetTheme;
 
   if (currentTheme === "light") {
-      targetTheme = "dark";
-  }else{
-     targetTheme = "light";
+    targetTheme = "dark";
+  } else {
+    targetTheme = "light";
   }
 
-  document.documentElement.setAttribute('data-theme', targetTheme)
-  localStorage.setItem('theme', targetTheme);
-}
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
+};
 
 input.addEventListener("keyup", inputBlur);
 mainDiv.addEventListener("keypress", buttonHandler);
-darkMode.addEventListener('click',darkModeHandler)
-
+darkMode.addEventListener("click", darkModeHandler);
